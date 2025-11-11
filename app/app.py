26f7406809db72595bd945aa034df7b46516aef4
khalilrmilA <khalil.rmila@esprit.tn>
2025-11-11 23:47:01 +0100

    Update debug print statement for Jenkins CI trigger in get_db function

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ DATABASE = './app.db'
 
 def get_db():
     db = getattr(g, '_database', None)
-    print("✅ Testing Jenkins CI trigger")
+    print("✅ Testing Jenkins CI trigger2")
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
